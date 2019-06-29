d3caaef84adb7672f4c847dabc6e0e8a31c109b5
Zach  Smith <zdsmith001@gmail.com>
2019-06-29 13:37:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,13 @@
 def position_taken?(board, index)
+<<<<<<< HEAD
   if board[index] ==  " " || board[index] == "" || board[index] == nil
     false
   else board[index] == "X" || board[index] == "O"
+=======
+  if board[index] == " "||""
+    false
+  elsif board[index] == "X" or "O"
+>>>>>>> 00ddb5c008891ec069f86702242d262bdf0a7256
     true
   end
 end
